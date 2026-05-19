10b4d129b5686d3f20750c4919f4e8a7a78a7f57
Dr Mark O'Connor <dr.mark.oconnor@googlemail.com>
2026-05-19 17:44:33 +0100

    Fix UUID validation for task delete

--- src/app/api/practice-tasks/[taskId]/route.ts
+++ src/app/api/practice-tasks/[taskId]/route.ts
@@ -21,7 +21,7 @@ const practiceStatuses = new Set<PracticeStatus>([
 ]);
 
 function isUuid(value: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
     value,
   );
 }
